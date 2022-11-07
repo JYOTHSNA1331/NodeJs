6323245b84968408a9f6cba9f28d848068903804
JYOTHSNA1331 <60293594+JYOTHSNA1331@users.noreply.github.com>
2022-11-07 19:54:55 +0530

    First Handson

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const server = http.createServer((req,res)=>{
+    res.write('<h1>Hello World</h1>')
+    res.write(JSON.stringify(data))
+})
+
+const data = {
+    fname:'John',
+    lname:'Smith'
+}
+
+server.listen(3000)
+
+server.on('connection',(socket)=>{
+    console.log('server connected')
+})
